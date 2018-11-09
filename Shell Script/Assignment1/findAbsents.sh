9b6cf2478475648370987a5f7264756f0c4d7d8f
Subangkar-Gitkraken-Ubuntu18-HP <subangkar.karmaker@gmail.com>
2018-11-09 19:28:53 +0600

    absent list Done

--- Shell Script/Assignment1/findAbsents.sh
+++ Shell Script/Assignment1/findAbsents.sh
@@ -1,17 +1,45 @@
 #!/bin/bash
 
-# cd output
+#findFiles
+digit='[0-9]'
+integer="$digit\{1,\}"
 
-# Strip leading and trailing white space (new line inclusive).
-# trim(){
-#     [[ "$1" =~ [^[:space:]](.*[^[:space:]])? ]]
-#     # printf "%s" "$BASH_REMATCH"
-# }
+stdname="[^_]\{1,\}"
+# stdid=$1;#"$digit\{7\}"
+# echo $stdid
+# find output -type f -regextype sed -regex "$stdname\_$integer\_assignsubmission_file_$stdid.*\.zip"
+# find output -type f -regextype sed -not -regex "$stdname\_$integer\_assignsubmission_file_1405015.*\.zip"
+
+RET_VAL_BOOL=1
+function hasStdID() {
+  stdid=$1;
+  res=`find output -type f -regextype sed -regex "$stdname\_$integer\_assignsubmission_file_$stdid.*\.zip"`
+  if [ -z "$res" ]; then
+    # echo 'No'
+    RET_VAL_BOOL=0;
+  else
+    # echo 'Yes'
+    RET_VAL_BOOL=1;
+  fi
+}
+
+# hasStdID $1
+#####
+
+ABS_FILE="output/log/absent.txt"
+ROSTER_FILE="CSE_322.csv"
+> "$ABS_FILE"
 
 IFS=$'\n'
 for student in `cut -d '"' -f 2-3 CSE_322.csv`
 do
       sid=$(echo $student| cut -d'"' -f 1|cut -f 2)
       sname=$(echo $student| cut -d',' -f 2)
-      echo $sid
+      # echo $sid
+      hasStdID $sid
+      echo "$sid,$sname,$RET_VAL_BOOL" >> "$ABS_FILE"
 done
+
+# function genAbsList() {
+#   #statements
+# }
--- Shell Script/Assignment1/findFiles.sh
+++ Shell Script/Assignment1/findFiles.sh
@@ -4,6 +4,23 @@ digit='[0-9]'
 integer="$digit\{1,\}"
 
 stdname="[^_]\{1,\}"
-stdid="$digit\{7\}"
+# stdid=$1;#"$digit\{7\}"
+# echo $stdid
+# find output -type f -regextype sed -regex "$stdname\_$integer\_assignsubmission_file_$stdid.*\.zip"
+# find output -type f -regextype sed -not -regex "$stdname\_$integer\_assignsubmission_file_1405015.*\.zip"
 
-find workingDir -type f -regextype sed -not -regex "$stdname\_$integer\_assignsubmission_file_$stdid.*\.zip"
+RET_VAL_BOOL=1
+function hasStdID() {
+  stdid=$1;
+  res=`find output -type f -regextype sed -regex "$stdname\_$integer\_assignsubmission_file_$stdid.*\.zip"`
+  if [ -z "$res" ]; then
+    # echo 'No'
+    RET_VAL_BOOL=0;
+  else
+    # echo 'Yes'
+    RET_VAL_BOOL=1;
+  fi
+}
+
+hasStdID $1
+# echo $RET_VAL_BOOL
