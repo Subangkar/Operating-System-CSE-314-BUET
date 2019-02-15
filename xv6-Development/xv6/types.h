5394f61debe87f22ea5d55f9ecdce6fbe9a8d9d4
Subangkar-GitKraken-Win10-HP <subangkar.karmaker@gmail.com>
2019-02-15 09:07:49 +0600

    merge error fixed

--- xv6-Development/xv6/types.h
+++ xv6-Development/xv6/types.h
@@ -2,7 +2,6 @@ typedef unsigned int   uint;
 typedef unsigned short ushort;
 typedef unsigned char  uchar;
 typedef uint pde_t;
-typedef enum {false,true} bool;
 typedef uint addr_t;
 
 typedef enum {false,true} bool;
